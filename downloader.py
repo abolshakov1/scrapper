59b9785ba11c43c6d4081a50e7b06e20d51abe7f
abolshakov1 <Anastasia7783468>
2021-08-08 20:36:02 +0300

    Finished downloader, dtf scrapper

--- downloader.py
+++ downloader.py
@@ -1,16 +1,23 @@
 import shutil
-
+import json
 import requests
+import wget
 
+import os, sys
 
 def download_image(url) -> str:
+
+    # application_path = os.path.dirname(sys.executable)
+
+    path = os.path.expanduser('~/Documents/scrapper/images/')
+
     r = requests.get(url, stream=True)
     try:
         name = url.split('/')[-2] + ".jpg"
         if r.status_code == 200:
-            with open(".\\images\\" + name, 'wb') as file:
+            with open(path + name, 'wb+') as file:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, file)
-                print('Image hase been saved successfully')
+                print('Image hase been saved successfully', file)
     except Exception as exc:
         print (exc)
--- scrappers/dtf/dtf.py
+++ scrappers/dtf/dtf.py
@@ -1,8 +1,11 @@
 import requests
+from datetime import datetime
+import json
 from bs4 import BeautifulSoup as _bs
 
-# from downloader import download_image
+from downloader import download_image
 from scrappers.scrapper import Scrapper
+from stats.stats_helper import get_stats_from_file, save_to_file
 
 class DtfScrapper(Scrapper):
     url = "https://dtf.ru/kek/"
@@ -12,6 +15,13 @@ class DtfScrapper(Scrapper):
     page = 1
     last_id = None
     last_sorting_value = None
+    min_likes_to_download = 30
+
+    stats = None
+    new_imgs = []
+
+    def __init__(self):
+        self.stats = get_stats_from_file()
 
     def scrap_best_pages(self, count):
         for i in range(count):
@@ -28,6 +38,8 @@ class DtfScrapper(Scrapper):
             content = self._get_page_content(url, first_page)
             self._parse(content, first_page)
             self.page += 1
+
+        save_to_file(self.stats)
 
     def _get_last_sorting_value_from_first_page(self, soup):
         last_sorting_value_div = soup.find('div', class_='feed')
@@ -81,7 +93,34 @@ class DtfScrapper(Scrapper):
                 self.last_id = data_id
                 print(data_id, img_link, likes)
 
+                if likes < self.min_likes_to_download:
+                    print (f'Skipped{data_id} not enought likes {likes}')
+                    continue
+                if str(data_id) in self.stats or \
+                   self.stats.get(str(data_id), {}).get('downloaded', False):
+                   print(f'Skipped {data_id} already in list')
+                   continue
+
+                self._update_record(data_id, img_link, likes)
         print("==================================================================================")
+        self._download_new_imgs()
+
+    def _update_record(self, data_id, img_link, likes):
+        self.stats[data_id] = { 'link': img_link,
+                                'likes': likes,
+                                'updated_at': datetime.now().ctime(),
+                                'processed': False,
+                                'downloaded': False}
+        self.new_imgs.append(data_id)
+
+    def _download_new_imgs(self):
+        for id_ in self.new_imgs:
+            url = self.stats[id_]['link']
+            download_image(url)
+            self.stats[id_]['downloaded'] = True
+
+        self.new_imgs.clear()
+
 
     def _get_page_content(self, url, firstPage=False):
         page = requests.get(url)
