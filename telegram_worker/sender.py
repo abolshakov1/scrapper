15a6b416f6c92cdc4f6fb8fa2e0eadaaef6fcb19
abolshakov1 <zloyeg@gmail.com>
2021-08-14 14:24:27 +0300

    Added limiter for send unprocessed imgs, managing db connection

--- telegram_worker/sender.py
+++ telegram_worker/sender.py
@@ -1,6 +1,6 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Bot
 
-from db_layer.db import db
+from db_layer.db import DbConnection
 from telegram_worker.settings import hurma_id, bot_token
 from utils import Singleton
 import time
@@ -8,6 +8,7 @@ import time
 
 class ScrapperTelegramBot(metaclass=Singleton):
     bot: Bot = Bot(token=bot_token)
+    db = DbConnection()
 
     def _send_photo(self, data_id: str, img_link: str, chat_id: str = hurma_id):
         btn = InlineKeyboardButton(text='like', callback_data=data_id)
@@ -21,32 +22,42 @@ class ScrapperTelegramBot(metaclass=Singleton):
         def decorate(self, *args, **kwargs):
             self.bot.sendMessage(chat_id=hurma_id,
                                  text='New images for approve')
-            time.sleep(1.5)
+            time.sleep(1)
             self.bot.sendMessage(chat_id=hurma_id,
                                  text='=================================')
-            time.sleep(1.5)
+            time.sleep(1)
 
             result = func(self, *args, **kwargs)
 
-            time.sleep(1.5)
+            time.sleep(1)
             self.bot.sendMessage(chat_id=hurma_id,
                                  text='=================================')
             return result
         return decorate
 
     @_delimiter_decorator
-    def send_unprocessed_img_for_approve(self):
-        unprocessed = db.select_processed_by_me(processed=False)
+    def send_unprocessed_img_for_approve(self, limiter=1):
+        self.db.open_connection()
+        unprocessed = self.db.select_unprocessed_and_unsent()
+        sent = []
 
-        i = 0
-        for data in unprocessed:
-            if i > 0:
-                break
+        try:
+            i = 0
+            for data in unprocessed:
+                if limiter and i >= limiter:
+                    break
 
-            data_id, link = data
-            self._send_photo(data_id, link)
-            time.sleep(1.5)
-            i += 1
+                data_id, link = data
+                self._send_photo(data_id, link)
+                time.sleep(1.5)
+                i += 1
+                sent.append((int(data_id), ))
+
+            self.db.update_sent(sent)
+        except Exception as exc:
+            print ("Some error on send unprocessed images ", exc)
+        finally:
+            self.db.close_connection()
 
 
 if __name__ == "__main__":
